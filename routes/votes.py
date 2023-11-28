4792720373a391c68d4dc619e8406fe53e00e2b4
Donald Peters <donny9585@gmail.com>
2023-11-28 01:15:25 +0100

    Figured out how to add radio buttons to the candidate list; will do a proper implementation in the morning.

--- routes/votes.py
+++ routes/votes.py
@@ -2,11 +2,11 @@ from flask_login import login_required
 
 from config import app
 from flask import render_template
-from models import Positions
+from models import Positions, Candidate
 
 
 @app.route('/votes')
 @login_required
 def votes():
     positions = Positions.query.all()
-    return render_template('vote.html', positions=positions)
+    return render_template('vote.html', positions=positions, Candidate=Candidate)
