041fad4661c91a2fdefad00b3091455d45341ff8
Donald Peters <donny9585@gmail.com>
2023-11-28 12:40:00 +0100

    Implemented the radio buttons correctly on the voting form

--- routes/votes.py
+++ routes/votes.py
@@ -1,7 +1,7 @@
 from flask_login import login_required
 
 from config import app
-from flask import render_template
+from flask import render_template, request
 from models import Positions, Candidate
 
 
@@ -10,3 +10,9 @@ from models import Positions, Candidate
 def votes():
     positions = Positions.query.all()
     return render_template('vote.html', positions=positions, Candidate=Candidate)
+
+
+@app.route('/cast-vote', methods=['GET', 'POST'])
+@login_required
+def cast_vote():
+    ...
