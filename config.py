5e4719bd4a025118e9c82b8e75f7ee8e0717c0e2
Donald Peters <donny9585@gmail.com>
2023-12-09 21:03:24 +0100

    wsgi good to go

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import os
 from os import getenv
 import secrets
 from flask import Flask, session, jsonify
@@ -9,8 +10,10 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect, generate_csrf
+from dotenv import load_dotenv
 import logging
 
+load_dotenv()
 user = getenv('VOTEBUDDY_USER')
 password = getenv('VOTEBUDDY_PWD')
 database = getenv('VOTEBUDDY_DB')
@@ -18,9 +21,12 @@ host = getenv('VOTEBUDDY_HOST')
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '2418a51bfc930c04eac5d264b84806c6'
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqldb://{user}:{password}@{host}/{database}'
+app.config['DEBUG'] = os.environ.get('FLASK_DEBUG')
 
+app.config['SECRET_KEY'] = '2418a51bfc930c04eac5d264b84806c6'
+# app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqldb://{user}:{password}@{host}/{database}'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://vote_buddy:vote_buddy_123@/Votebuddy?unix_socket=/cloudsql' \
+                                        '/votebuddy-407411:europe-west1:vote-buddy'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,4 @@
+from config import app
 
+if __name__ == '__main__':
+    app.run()
