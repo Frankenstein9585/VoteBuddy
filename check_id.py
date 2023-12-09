fbddfd12f12f39db22437d04f4e3353bfc4472fc
Donald Peters <donny9585@gmail.com>
2023-12-09 15:41:12 +0100

    edited check_id.py

--- check_id.py
+++ check_id.py
@@ -1,13 +1,6 @@
-from config import User, app
-from gcloud_ocr import detect_text
-
-
-# with app.app_context():
-#     students = User.query.all()
-#
-# matric_numbers = [student.matric_number for student in students]
-# print(matric_numbers)
 def check_id(img_path, matric_number):
+    from config import User
+    from gcloud_ocr import detect_text
     student_info = detect_text(img_path)
     if 'Student Identification Card' in student_info:
         if matric_number in student_info:
