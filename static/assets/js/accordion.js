2d48a14f584605c09bc9a7377bec14847425f233
Donald Peters <donny9585@gmail.com>
2023-12-10 13:40:19 +0100

    Adjusted Vote form functionality

---
+++ static/assets/js/accordion.js
@@ -0,0 +1,25 @@
+  document.addEventListener('DOMContentLoaded', function () {
+    var accordion = new bootstrap.Collapse(document.getElementById('accordionExample'), {
+      toggle: false
+    });
+
+    accordion._element.addEventListener('show.bs.collapse', function (event) {
+      // Close the previously open accordion item
+      var previouslyOpenItem = accordion._element.querySelector('.accordion-item:not(.collapsed)');
+      if (previouslyOpenItem && previouslyOpenItem !== event.target.parentElement) {
+        accordion.hide(previouslyOpenItem.querySelector('.accordion-collapse'));
+      }
+
+      // Scroll to the top of the opened accordion item's button
+      var openedItemButton = event.target.closest('.accordion-item').querySelector('.accordion-button');
+      if (openedItemButton) {
+        setTimeout(function() {
+          openedItemButton.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+            inline: 'start' // This ensures the left edge of the element is aligned with the left edge of the visible area of the scrollable ancestor
+          });
+        }, 350); // Adjust the delay as needed
+      }
+    });
+  });
