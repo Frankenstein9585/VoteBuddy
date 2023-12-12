cdbfca3fbbdc844596ef2be914dccdb7d463504c
Donald Peters <donny9585@gmail.com>
2023-12-12 14:28:19 +0100

    Fixed ID Verification

--- utils/check_id.py
+++ utils/check_id.py
@@ -6,7 +6,6 @@ def check_id(img_path, matric_number):
     if not student_info:
         return None
     if 'Student Identification Card'.casefold() in student_info:
-        print('SIC found')
         if matric_number.casefold() in student_info:
             user = User.find_obj_by(matric_number=matric_number)
             if user:
