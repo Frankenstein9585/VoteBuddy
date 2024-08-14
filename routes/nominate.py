220af6cb3e8ed0367f7102698314b87e78f3eff5
Donald Peters <donny9585@gmail.com>
2024-08-14 22:03:01 +0100

    Added indexes to positions to make the display in the correct order

--- routes/nominate.py
+++ routes/nominate.py
@@ -4,6 +4,12 @@ from flask import render_template, request, redirect, url_for, jsonify
 from models import Positions, User
 
 
+@app.route('/nominees')
+def nominate():
+    positions = Positions.query.order_by(Positions.index).all()
+    return render_template('nominees.html', positions=positions)
+
+
 @app.route('/autocomplete', methods=['GET'])
 def autocomplete():
     search = request.args.get('q')
@@ -11,9 +17,3 @@ def autocomplete():
     suggestions = [{'id': user.id, 'label': user.name.title()} for user in results]
     print(suggestions)
     return jsonify(suggestions)
-
-
-@app.route('/nominate')
-def nominate():
-    positions = Positions.query.all()
-    return render_template('nominate.html', positions=positions)
